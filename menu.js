876cfcee0e989ccacfd5d9878ff7ea3c32340a06
Anthony <58270745+MxThony@users.noreply.github.com>
2024-03-04 00:01:19 -0300

    atualizando

---
+++ menu.js
@@ -0,0 +1,6 @@
+let menuCelular = document.getElementById('menu-celular')
+let menu = document.getElementById('menu-mobile')
+
+menuCelular.addEventListener('click',()=>{
+    menu.classList.add('abrindo-menu')
+})
